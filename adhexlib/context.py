12be4909115938719fd195b1ff85bda18749e31d
A_D <africandeath@gmail.com>
2017-06-14 18:26:27 +0200

    removed adhexlib

--- adhexlib/context.py
+++
@@ -1,9 +0,0 @@
-import hexchat
-
-
-def printtocontext(name, msg):
-    if hexchat.find_context(hexchat.get_info("network"), name):
-        hexchat.find_context(hexchat.get_info("network"), name).prnt(msg)
-    else:
-        hexchat.command("QUERY -nofocus {}".format(name))
-        hexchat.find_context(hexchat.get_info("network"), name).prnt(msg)
