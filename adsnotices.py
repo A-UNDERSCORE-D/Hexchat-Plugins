80f106e3761e9aeccf879d1ee363f7388d77c10f
A_D <africandeath@gmail.com>
2017-07-09 19:39:02 +0200

    Moved to using decorators to define commands, and added a help command

--- adsnotices.py
+++ adsnotices.py
@@ -1,8 +1,9 @@
+import inspect
 import json
 import re
 import subprocess
 import time
-
+from collections import OrderedDict
 import hexchat
 
 __module_name__ = "adsnotices"
@@ -23,7 +24,6 @@ blockvisual_DEFAULT = {}
 whois_timeout_DEFAULT = 180.0
 snote_timeout_DEFAULT = 1.0
 snote_specific_timeout_DEFAULT = {}
-
 
 allowednets = allowednets_DEFAULT
 sendvisual = sendvisual_DEFAULT
@@ -59,10 +59,8 @@ def onsnotice(word, word_eol, userdata):
             if re.match(snoteregex.format(snotes[mask]), notice):
                 printtocontext(">>{}<<".format(mask), notice)
                 lowermask = mask.lower()
-                if lowermask in sendvisual:
-                    if checktimout(lowermask):
-                        sendnotif(notice, lowermask)
-
+                if lowermask in sendvisual and checktimout(lowermask):
+                    sendnotif(notice, lowermask)
                 eat = True
 
         whois = whoisregex.match(notice)
@@ -75,9 +73,10 @@ def onsnotice(word, word_eol, userdata):
 
 
 def checktimout(lowermask):
-    if lowermask in snote_timers and (time.time() - snote_timers[lowermask]) >= snote_specific_timeout.get(lowermask, snote_timeout):
-        snote_timers[lowermask] = time.time()
-        return True
+    if lowermask in snote_timers:
+        if (time.time() - snote_timers[lowermask]) >= snote_specific_timeout.get(lowermask, snote_timeout):
+            snote_timers[lowermask] = time.time()
+            return True
     elif lowermask not in snote_timers:
         snote_timers[lowermask] = time.time()
         return True
@@ -85,9 +84,8 @@ def checktimout(lowermask):
 
 
 def sendwhoisnotice(msg):
-    hexchat.command("RECV :whois!whois@whois NOTICE "
-                    "{mynick} :{nmsg}".format(mynick=hexchat.get_info("nick"),
-                                              nmsg=" ".join(msg.groups())))
+    hexchat.command("RECV :whois!whois@whois NOTICE {mynick} :{nmsg}".format(mynick=hexchat.get_info("nick"),
+                                                                             nmsg=" ".join(msg.groups())))
 
 
 def counterwhois(nick):
@@ -136,12 +134,27 @@ def procleanup(userdata):
     return True
 
 
+commands = OrderedDict()
+
+
+def command(cmd, cmdargs="", cmdhelp=""):
+    def _decorate(func):
+        assert cmd not in commands
+        if cmdhelp:
+            commands[cmd] = (func, cmdargs, cmdhelp)
+        return func
+
+    return _decorate
+
+
+@command("addnet", "network name", "Adds a network to the list of networks where this script will function")
 def addnet(net):
     global allowednets
     allowednets.append(net)
     saveconfig()
 
 
+@command("delnet", "network name", "Removes a network from the list of networks where this script will function")
 def delnet(net):
     global allowednets
     if net in allowednets:
@@ -151,12 +164,19 @@ def delnet(net):
         print("{} is not in the allowed network list".format(net))
 
 
+@command("listnet", cmdhelp="Lists the networks where this script will function")
+def listnet():
+    print("networks:", ", ".join(allowednets))
+
+
+@command("addvisual", "snote", "Adds an snotice to the list of snotes that are sent to notify-send")
 def addvisual(snotice):
     global sendvisual
     sendvisual.append(snotice.lower())
     saveconfig()
 
 
+@command("delvisual", "snote", "Removes an snote from the list of snotes that are sent to notify send")
 def delvisual(snotice):
     global sendvisual
     snotice = snotice.lower()
@@ -167,6 +187,14 @@ def delvisual(snotice):
         print("{} is not in the list of visually send snotices".format(snotice))
 
 
+@command("listvisual", cmdhelp="Lists the snotees that are sent to notify-send")
+def listvisual():
+    print("Snotes that are sent visually:", ", ".join(sendvisual))
+
+
+@command("addblockvisual", "snote, phrase",
+         "Adds a phrase to the list of phrases that block a snote from being sent to notify-send. "
+         "You can use 'all' as the snote to check all snotices for a given phrase.")
 def addblockvisual(block):
     global blockvisual
     if len(block.split()) >= 2:
@@ -188,6 +216,8 @@ def addblockvisual(block):
         return
 
 
+@command("delblockvisual", "snote, phrase",
+         "Removes a phrase from the list of phrases that stop snotices from being sent to notify-send")
 def delblockvisual(block):
     global blockvisual
     if len(block.split()) >= 2:
@@ -198,7 +228,7 @@ def delblockvisual(block):
             if not blockvisual[key]:
                 del blockvisual[key]
             saveconfig()
-            print("'{}' added to {}'s block list".format(key, block))
+            print("'{}' removed from {}'s block list".format(key, block))
         else:
             print("{key} is not in the block visual list".format(key=key))
     else:
@@ -206,14 +236,17 @@ def delblockvisual(block):
         return
 
 
-def listblockvisual(*args, **kwargs):
+@command("listblockvisual", cmdhelp="Lists phrases that are blocked in snotices sent to notify-send")
+def listblockvisual():
     print("Strings blocked in snotices:")
     for ntype in blockvisual:
-        print(ntype, ";")
+        print(ntype + ";")
         for block in blockvisual[ntype]:
-            print(" `", block)
+            print(" `'{}'".format(block))
 
 
+@command("setwhoistimeout", "number",  "sets the global whois timeout. This is the minimum time between counterwhoises "
+                                       "for a given user in seconds (fractions of a second are supported)")
 def setwhoistimeout(timeout):
     global whois_timeout
     whois_timeout = float(timeout)
@@ -221,6 +254,14 @@ def setwhoistimeout(timeout):
     print("whois timeout set to {}".format(whois_timeout))
 
 
+@command("listwhoistimeout", cmdhelp="Prints the global whois timeout")
+def listwhoistimeout():
+    print("Whois timeout is: {}".format(whois_timeout))
+
+
+@command("setsnotetimeout", "number",
+         "Sets the global snotice timeout. This sets the minimum time between calls of notify-send in seconds "
+         "(fractions of a second are supported)")
 def setsnotetimeout(timeout):
     global snote_timeout
     snote_timeout = float(timeout)
@@ -228,6 +269,13 @@ def setsnotetimeout(timeout):
     print("snote timeout set to {}".format(snote_timeout))
 
 
+@command("listsnotetimeout", cmdhelp="Lists the global snotice timeout")
+def listsnotetimeout():
+    print("Snote timeout is: {}".format(snote_timeout))
+
+
+@command("addsecifictimeout", "snote, timeout",
+         "Adds overriding timeouts for a given snotice, these are used instead of the global one")
 def addspecifictimeout(timeout):
     timeout = timeout.lower()
     split = timeout.split()
@@ -237,6 +285,7 @@ def addspecifictimeout(timeout):
         saveconfig()
 
 
+@command("delspecifictimeout", "snote, timeout", "Removes a set specific snote timeout")
 def delspecifictimeout(timeout):
     timeout = timeout.lower()
     split = timeout.split()
@@ -244,6 +293,43 @@ def delspecifictimeout(timeout):
         del snote_specific_timeout[split[0]]
         saveconfig()
         print("{}'s specific timeout has been removed".format(split[0]))
+
+
+@command("listspecifictimeout", cmdhelp="lists the specific timeouts set for snotices")
+def listspecifictimeout():
+    if snote_specific_timeout:
+        print("Specific Snote Timeouts are as follows:")
+        for k, v in snote_specific_timeout.items():
+            print(k + ":", v)
+    else:
+        print("You have set no specific snote timeouts")
+
+
+@command("help", cmdhelp="Shows help")
+def commandhelp():
+    printfmt = "{cmd}\t {args:<15} | {desc:<50}"
+    print("{cmd:}\t {args:^15} | {desc:^50}".format(cmd="\x02Command\x02", args="Args", desc="Description"))
+    print("-"*110)
+    for cmdname in commands:
+        cmd = commands[cmdname]
+        cmdargs = cmd[1] if cmd[1] else "None"
+        cmdhelp = cmd[2] if cmd[2] else "No description provided"
+        cmdprint = "\x02" + cmdname + "\x02"
+        print(printfmt.format(cmd=cmdprint, args=cmdargs, desc=cmdhelp))
+        print("-" * 110)
+
+
+@command("debug", cmdhelp="debug command, lists a few variables and their types")
+def debug():
+    print("allowednets: {} type: {}".format(allowednets, type(allowednets)))
+    print("sendvisual: {} type: {}".format(sendvisual, type(sendvisual)))
+    print("blocksendvisual: {} type: {}".format(blockvisual, type(blockvisual)))
+    print("whoistimeout: {} type: {}".format(whois_timeout, type(whois_timeout)))
+    print("snotetimeout: {} type: {}".format(snote_timeout, type(snote_timeout)))
+    print("snote timers: {} type: {}".format(snote_timers, type(snote_timers)))
+    print("whois timers: {} type: {}".format(users, type(users)))
+    print("specific timers: {} type {}".format(snote_specific_timeout, type(snote_specific_timeout)))
+    print("Commands: {}".format(commands))
 
 
 def getconfig():
@@ -272,47 +358,25 @@ def saveconfig():
     hexchat.set_pluginpref(__module_name__ + "_config", json.dumps(config))
 
 
-def debug(*args):
-    print("allowednets: {} type: {}".format(allowednets, type(allowednets)))
-    print("sendvisual: {} type: {}".format(sendvisual, type(sendvisual)))
-    print("blocksendvisual: {} type: {}".format(blockvisual, type(blockvisual)))
-    print("whoistimeout: {} type: {}".format(whois_timeout, type(whois_timeout)))
-    print("snotetimeout: {} type: {}".format(snote_timeout, type(snote_timeout)))
-    print("snote timers: {} type: {}".format(snote_timers, type(snote_timers)))
-    print("whois timers: {} type: {}".format(users, type(users)))
-    print("specific timers: {} type {}".format(snote_specific_timeout, type(snote_specific_timeout)))
-
-
-commands = {
-    "addnet": addnet,
-    "delnet": delnet,
-    "listnet": lambda x: print("networks:", ", ".join(allowednets)),
-
-    "addvisual": addvisual,
-    "delvisual": delvisual,
-    "listvisual": lambda x: print("Snotes that are sent visually:", ", ".join(sendvisual)),
-
-    "addblockvisual": addblockvisual,
-    "delblockvisual": delblockvisual,
-    "listblockvisual": listblockvisual,
-    "debug": debug,
-    "setwhoistimeout": setwhoistimeout,
-    "listwhoistimeout": lambda x: print("Whois timeout is: {}".format(whois_timeout)),
-    "setsnotetimeout": setsnotetimeout,
-    "listsnotetimeout": lambda x: print("Snote timeout is: {}".format(snote_timeout)),
-    "setspecifictimeout": addspecifictimeout,
-    "delspecifictimeout": delspecifictimeout,
-}
-
-
 def oncmd(word, word_eol, userdata):
-    if len(word) < 2:
-        hexchat.command("HELP SNOTE")
-    elif word[1].lower() in commands:
-        commands[word[1]]((" ".join(word[2:]) if len(word) >= 3 else None))
+    if len(word) >= 2:
+        cmd = commands.get(word[1].lower())
+        if cmd[0]:
+            signature = inspect.signature(cmd[0])
+            if len(signature.parameters) > 0:
+                if len(word) == 3:
+                    print("word is 3")
+                    cmd[0](word[2])
+                elif len(word) > 3:
+                    cmd[0](" ".join(word[2:]))
+                else:
+                    print("not enough args")
+
+            else:
+                cmd[0]()
     else:
         hexchat.command("HELP SNOTE")
-    return hexchat.EAT_ALL
+    return hexchat.EAT_HEXCHAT
 
 
 def printtocontext(name, msg):
@@ -333,7 +397,6 @@ def onunload(userdata):
 
 getconfig()
 hexchat.hook_print("Server Notice", onsnotice)
-hexchat.hook_command("SNOTE", oncmd, help="USAGE: /SNOTE ADD/DEL/LIST "
-                                          "NET|VISUAL|BLOCKVISUAL")
+hexchat.hook_command("SNOTE", oncmd, help="USAGE: for usage, run snote help")
 hexchat.hook_timer(15 * 1000, procleanup)
 print(__module_name__, "plugin loaded")
