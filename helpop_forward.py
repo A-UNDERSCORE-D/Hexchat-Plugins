04400e113d3b10f5e901539ba1d3031d70a56503
A_D <A_D@snoonet.org>
2017-08-16 04:49:34 +0200

    added helpop_forward. does what it says on the tin

---
+++ helpop_forward.py
@@ -0,0 +1,28 @@
+import hexchat
+
+__module_name__ = "helpop_forward"
+__module_version__ = "0.1"
+__module_description__ = "Pushes HELPOP responses into your current channel, rather than to the server window"
+
+hook = 0
+
+
+def numeric_cb(word, word_eol, userdata):
+    line = word_eol[3][1:]
+    print(line)
+    if line == "*** End of HELPOP":
+        hexchat.unhook(hook)
+    return hexchat.EAT_HEXCHAT
+
+
+def helpop_cb(word, word_eol, userdata):
+    global hook
+    hook = hexchat.hook_server("292", numeric_cb)
+
+
+@hexchat.hook_unload
+def onunload(userdata):
+    print(__module_name__, "unloaded")
+
+hexchat.hook_command("HELPOP", helpop_cb)
+print(__module_name__, "loaded")
