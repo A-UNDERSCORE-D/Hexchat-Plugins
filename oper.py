3e20d66b7d18410a322f51cc210420b693dbfce8
A_D <A_D@snoonet.org>
2017-08-20 07:07:35 +0200

    added some kill messages and did some small refactors

--- oper.py
+++ oper.py
@@ -17,12 +17,18 @@ kills_text = [
     "Here, I'm going to let you hold my axe for a second... oops, seems I dropped instead",
     "Here, I'm going to let you hold my axe for a second... oops, seems you are too weak to wield such things",
     "Here, I'm going to let you hold my axe for a second...",
+    "You are the weakest link. Goodbye. ;)"
 ]
 
 kills_images = [
     "http://i.imgur.com/O3DHIA5.gif",
     "http://i.imgur.com/A4XPmWT.gif",
     "http://i.imgur.com/Mu3tJ6T.jpg",
+    "https://i.imgur.com/R9WnseD.gif",
+    "https://i.imgur.com/xn4rF.gif",
+    "https://i.imgur.com/rnwF0.gif",
+    "https://i.imgur.com/g2Yo3.gif",
+    "https://i.imgur.com/Z3ha5dA.gif",
 ]
 
 kills = kills_text * 2 + kills_images
@@ -32,15 +38,10 @@ def kill(word, word_eol, userdata):
     if len(word) < 2:
         print("I need an argument, /okill nick [reason]")
     elif len(word) == 2:
-        hexchat.command(
-            "msg OperServ KILL {nick} {reason}".format(
-                nick=word[1], reason=random.choice(kills))
-        )
+        hexchat.command("msg OperServ KILL {nick} {reason}".format(nick=word[1], reason=random.choice(kills)))
     elif len(word) > 2:
         hexchat.command(
-            "msg OperServ KILL {nick} {reason}".format(
-                nick=word[1], reason=word_eol[2])
-        )
+            "msg OperServ KILL {nick} {reason}".format(nick=word[1], reason=word_eol[2]))
     return hexchat.EAT_ALL
 
 
