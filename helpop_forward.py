4f9ae240b328928883fa864e467f47ae9af9af7d
A_D <A_D@snoonet.org>
2017-08-20 07:08:19 +0200

    added the ability to have helpop appear as a msgbox

--- helpop_forward.py
+++ helpop_forward.py
@@ -5,14 +5,31 @@ __module_version__ = "0.1"
 __module_description__ = "Pushes HELPOP responses into your current channel, rather than to the server window"
 
 hook = 0
+msgbox = False
+helpop_line = []
+# TODO: Make this optionally use a dialog
 
 
 def numeric_cb(word, word_eol, userdata):
     line = word_eol[3][1:]
-    print(line)
+    if msgbox:
+        helpop_line.append(line)
+    else:
+        print(line)
     if line == "*** End of HELPOP":
         hexchat.unhook(hook)
+        if msgbox:
+            hexchat.command("GUI MSGBOX {}".format("\n".join(map(sanitise_line, helpop_line))))
+        helpop_line.clear()
     return hexchat.EAT_HEXCHAT
+
+
+def sanitise_line(line):
+    replchar = "\xa0"
+    if line:
+        return line.replace(" ", replchar)
+    else:
+        return replchar
 
 
 def helpop_cb(word, word_eol, userdata):
