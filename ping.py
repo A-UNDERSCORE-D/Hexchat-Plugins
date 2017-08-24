d023e15358e02f8ff1354d48f2f270a8d2dfe0dd
linuxdaemon <linuxdaemon@snoonet.org>
2017-08-23 22:57:12 -0500

    Refactor ping.py

--- ping.py
+++ ping.py
@@ -3,38 +3,57 @@ __module_name__ = "adping"
 __module_version__ = "2.0"
 __module_description__ = "writes pings and notices to an additional channel"
 
-
-def incomingping(word, word_eol, userdata):
-    hexchat.command("query -nofocus >>pings<<")
-    con = hexchat.find_context(server=hexchat.get_info("network"),
-                               channel=">>pings<<")
-    con.emit_print("Channel Message", word[0], word[1],
-                   "", "{}/".format(hexchat.get_info("channel")))
-    return hexchat.EAT_NONE
+WINDOW_FMT = ">>{name}<<"
+emitting_notice = False
+emitting_wallops = False
 
 
-def incomingaping(word, word_eol, userdata):
-    hexchat.command("query -nofocus >>pings<<")
-    con = hexchat.find_context(server=hexchat.get_info("network"),
-                               channel=">>pings<<")
-    con.emit_print("Channel Action", "{}/{}".format(
-        hexchat.get_info("channel"), word[0]), word[1], "", "")
-    return hexchat.EAT_NONE
+def get_window(name, server=None):
+    name = WINDOW_FMT.format(name=name)
+    if server is None:
+        server = hexchat.get_info("network")
+    ctx = hexchat.find_context(server=server, channel=name)
+    if ctx is None:
+        server_ctx = hexchat.find_context(server=server)
+        server_ctx.command("query -nofocus {}".format(name))
+        ctx = hexchat.find_context(server=server, channel=name)
+    return ctx
+
+
+def log_ping(event_type, nick, message, chan=None):
+    if chan is None:
+        chan = hexchat.get_info("channel")
+    con = get_window("pings")
+    source = "{}/{}".format(chan, nick)
+    con.emit_print(event_type, source, message, "", "")
+
+
+def ping_handler(event_type):
+    def _handle(word, word_eol, userdata):
+        log_ping(event_type, word[0], word[1])
+
+    return _handle
 
 
 def incomingnotice(word, word_eol, userdata):
-    hexchat.command("query -nofocus >>notices<<")
-    con = hexchat.find_context(server=hexchat.get_info("network"),
-                               channel=">>notices<<")
-    con.prnt("\00328-\00329{}\00328-\003\t{}".format(word[0], word[1]))
+    global emitting_notice
+    if emitting_notice:
+        return
+    con = get_window("notices")
+    emitting_notice = True
+    con.emit_print("Notice", word[0], word[1])
+    emitting_notice = False
     return hexchat.EAT_NONE
 
 
 def incomingwallops(word, word_eol, userdata):
-    hexchat.command("query -nofocus >>wallops<<")
-    con = hexchat.find_context(server=hexchat.get_info("network"),
-                               channel=">>wallops<<")
-    con.prnt("\00328-\00329{}/Wallops\00328-\003\t{}".format(word[0], word[1]))
+    global emitting_wallops
+    if emitting_wallops:
+        return
+    con = get_window("wallops")
+    emitting_wallops = True
+    con.emit_print("Receive Wallops", word[0], word[1])
+    emitting_wallops = False
     return hexchat.EAT_NONE
 
 
@@ -43,8 +62,9 @@ def onunload(userdata):
     print(__module_name__, "plugin unloaded")
 
 
-hexchat.hook_print("Channel Msg Hilight", incomingping)
-hexchat.hook_print("Channel Action Hilight", incomingaping)
+hexchat.hook_print("Channel Msg Hilight", ping_handler("Channel Message"))
+hexchat.hook_print("Channel Action Hilight", ping_handler("Channel Action"))
 hexchat.hook_print("Notice", incomingnotice)
 hexchat.hook_print("Receive Wallops", incomingwallops)
 print(__module_name__, "plugin loaded")
+
