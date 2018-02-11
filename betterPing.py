712f79dfaa5a14e587e5be122c83c09dcbdb27f3
A_D <A_D@snoonet.org>
2018-02-11 09:31:24 +0200

    added command backbone, fixed save and get funcs

--- betterPing.py
+++ betterPing.py
@@ -1,6 +1,8 @@
 import re
+import pickle
 from fnmatch import fnmatch, fnmatchcase
 from pathlib import Path
+from collections import namedtuple
 
 import hexchat
 
@@ -22,8 +24,14 @@ class Checker:
     def __init__(self, check_str, case_sensitive=False, networks=None, channels=None):
         self.str = check_str
         self.type = "contains"
+        if networks is None:
+            networks = []
         self.nets = networks
+
+        if channels is None:
+            channels = []
         self.chans = channels
+
         self.case_sensitive = case_sensitive
 
     def check(self, str_to_check):
@@ -31,9 +39,8 @@ class Checker:
         cur_net = hexchat.get_info("network")
 
         # TODO: This could cause slowdowns due to iteration. Could checking this once globally be better?
-        if not (self.nets is not None and any((True for net in self.nets if net.casefold() == cur_net))):
-            return False
-        if not (self.chans is not None and any((True for chan in self.nets if chan.casefold() == cur_chan))):
+        if not any(net.casefold() == cur_net for net in self.nets) or not \
+                any(chan.casefold() == cur_chan for chan in self.chans):
             return False
 
         return self._check(str_to_check)
@@ -92,14 +99,90 @@ class ExactChecker(Checker):
         return self.str.casefold() == str_to_check.casefold()
 
 
-def saveconfig(conf=config):
-    with config_file.open("w") as f:
-        ...
+def save_checkers():
+    with config_file.open("wb") as f:
+        pickle.dump(checkers, f)
 
 
-def getconfig():
+def get_checkers():
     if not config_file.exists():
-        ...
+        return checkers
+    with config_file.open("rb") as f:
+
+        return pickle.load(f)
+
+
+# Start of hexchat commands etc.
+commands = {}
+command_tuple = namedtuple("command_tuple", "func help_text")
+
+
+# Because Im lazy and I think this helps readability.
+def command(cmd, min_args=1, usage="{cmd} requires at least {count_args} arguments", subcommand=True, help_msg=""):
+    cmd = cmd.upper()
+
+    def _decorate(f):
+        def _check_args(word, word_eol, userdata):
+            if len(word) < min_args:
+                print(usage.format(cmd=cmd, count_args=min_args))
+                return hexchat.EAT_ALL
+
+            ret = f(word, word_eol, userdata)
+            if ret is not None:
+                return hexchat.EAT_ALL
+
+            return ret
+        if not subcommand:
+            hexchat.hook_command(cmd, _check_args)
+        else:
+            assert cmd not in commands
+            commands[cmd] = command_tuple(f, help_msg)
+        return _check_args
+
+    return _decorate
+
+
+@command("ping", 2, subcommand=False)
+def main_command(word, word_eol, userdata):
+    # No need to check length because that is done for me, to a point, anyway
+    cmd = word[1].upper()
+    if cmd in commands:
+        commands[cmd].func(word[1:], word_eol[1:], userdata)
+    else:
+        print("Unknown command {}, try /ping help for a list of commands".format(cmd))
+
+
+def msg_hook(f):
+    hexchat.hook_print("Channel Message", f)
+    hexchat.hook_print("Private Message", f)
+    hexchat.hook_print("Private Message to Dialog", f)
+
+
+@command("debug")
+def debug_cb(word, word_eol, userdata):
+    for checker in checkers:
+        print(checker)
+
+
+@command("help")
+def help_cb(word, word_eol, userdata):
+    if len(word) > 1:
+        cmd = word[1].upper()
+        if cmd not in commands:
+            print("Unknown command")
+            return
+
+        print("help for {command}: {help_text}".format(command=cmd, help_text=commands[cmd].help_text))
+
+    else:
+        print("Available commands:")
+        for cmd in commands:
+            print("{cmd:<10} | {help_text}".format(cmd=cmd, help_text=commands[cmd].help_text))
+
+
+@command("addchecker")
+def add_cb(word, word_eol, userdata):
+    ...
 
 
 @hexchat.hook_unload
