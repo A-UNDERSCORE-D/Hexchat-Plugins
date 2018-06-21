11277339ebcab195ceb25b0a4f4b881d97d7682f
A_D <A_D@snoonet.org>
2018-06-21 06:50:16 +0200

    rewrote spamsafety

--- spamsafety.lua
+++ spamsafety.lua
@@ -1,110 +1,106 @@
-local plugin_name = "spamSaftey"
-local plugin_description = "Removes laggy things when being pingspammed"
-local plugin_version = "0.1"
+local plugin_name = "spamSafety2"
+local plugin_description = "Rewrite of spamSafety, removes weird bugs"
+local plugin_version = "2.0"
 
 hexchat.register(plugin_name, plugin_version, plugin_description)
 
-local last = os.time()
-local count = {0, 0}
-local max = {30, 50}
-local isoff = {false, false}
-local wait = {(1000 * 60) * 1, (1000 * 60) * 1}
-local timer = 0
 
-local function cleanup(t)
-    if timer ~= 0 then
-    print("Removing timer and resetting to defauts")
-    hexchat.unhook(timer)
-    if t then
-      count[t] = 0
-    else
-      count = {0, 0}
-    end
-    isoff[t] = false
-    timer = 0
+local watchers = {}
+local WatcherClass = {}
+WatcherClass.__index = WatcherClass
+
+function WatcherClass:new(wname, ts, wmessage)
+  local ret = {
+    name = wname,
+    last = os.time(),
+    max_count = 20,
+    lock_time = 20,
+    timeout = 2,
+    count = 0,
+    timer = 0,
+    to_set = ts,
+    triggered = false,
+    message = wmessage,
+    disable_timer_hook = 0
+  }
+  setmetatable(ret, WatcherClass)
+  return ret
+end
+
+
+function WatcherClass:print_table()
+  for k, v in pairs(self) do
+    print(string.format("%s = %s", k, v))
   end
 end
 
-local function nopms()
-  hexchat.command("SET input_balloon_priv 0")
-  hexchat.command("SET input_beep_priv 0")
-  hexchat.command("SET input_flash_priv 0")
-  hexchat.command("SET input_tray_priv 0")
-  return hexchat.EAT_HEXCHAT
-end
 
-local function nohilights()
-  hexchat.command("SET input_balloon_hilight 0")
-  hexchat.command("SET input_beep_hilight 0")
-  hexchat.command("SET input_flash_hilight 0")
-  hexchat.command("SET input_tray_hilight 0")
-  return hexchat.EAT_HEXCHAT
-end
-
-local function yespms(t)
-  hexchat.command("SET input_balloon_priv 1")
-  hexchat.command("SET input_beep_priv 1")
-  hexchat.command("SET input_flash_priv 1")
-  hexchat.command("SET input_tray_priv 1")
-  cleanup(t)
-  return hexchat.EAT_HEXCHAT
-end
-
-local function yeshilights(t)
-  hexchat.command("SET input_balloon_hilight 1")
-  hexchat.command("SET input_beep_hilight 1")
-  hexchat.command("SET input_flash_hilight 1")
-  hexchat.command("SET input_tray_hilight 1")
-  cleanup(t)
-  return hexchat.EAT_HEXCHAT
-end
-
-local function timercb(t)
-  hexchat.find_context(hexchat.get_info("network"), nil):set()
-  if t == 1 then
-    yeshilights(t)
-  elseif t == 2 then
-    yespms(t)
+function WatcherClass:run_set(state)
+  local to_set = "1"
+  if state ~= true then to_set = "0" end
+  for _, v in ipairs(self.to_set) do
+    hexchat.command(string.format("SET -quiet %s %s", v, to_set))
   end
-  print("Timer expired restoring standard behaviour")
-  count[t] = 0
-  isoff[t] = false
-  return false
 end
 
-local function onPing(t)
-  if (not isoff[t]) and (os.time() - last) < 10 then
-    count[t] = count[t] + 1
-  else
-    count[t] = 0
-  end
-  if (not isoff[t]) and (count[t] > max[t]) then
-    hexchat.find_context(hexchat.get_info("network"), nil):set()
-    if t == 1 then
-      print("You are being ping spammed. in order to prevent client lag ping sounds and notifications have been disabled.")
-      nohilights(t)
-    elseif t == 2 then
-      print("You are being PM spammed")
-      nopms(t)
-    end
-    timer = hexchat.hook_timer(wait[t], function() timercb(t) end)
-    count[t] = 0
-    isoff[t] = true
-  end
-  last = os.time()
+function WatcherClass:enable()
+  self.disable_timer_hook = hexchat.hook_timer(self.lock_time * 1000, function() self:disable() self.disable_timer_hook = 0 return false end)
+  self:run_set(false)
+  self.triggered = true
 end
 
-hexchat.hook_unload(function()
-  print(plugin_name .. " unloaded")
-end)
+function WatcherClass:disable()
+  if not self.triggered then print("already disabled") return end
+  self:run_set(true)
+  self.triggered = false
+  self.count = 0
+  if self.disable_timer_hook ~= 0 then
+    hexchat.unhook(self.disable_timer_hook)
+    self.disable_timer_hook = 0
+  end
+  print("timer disabled")
+end
 
-hexchat.hook_command("PINGSPAMON", yeshilights)
-hexchat.hook_command("PINGSPAMOFF", nohilights)
-hexchat.hook_command("PMSPAMON", yespms)
-hexchat.hook_command("PMSPAMOFF", nopms)
+function WatcherClass:check()
+  if self.count >= self.max_count then
+    print(self.message)
+    self:enable()
+  end
+end
 
-hexchat.hook_print("Channel Msg Hilight", function() onPing(1) end)
-hexchat.hook_print("Channel Action Hilight", function() onPing(1) end)
-hexchat.hook_print("Private Message to Dialog", function() onPing(2) end)
-hexchat.hook_print("Private Action to Dialog", function() onPing(2) end)
-print(plugin_name .. " loaded")
+function WatcherClass:trigger()
+  local curtime = os.time()
+  if curtime - self.last < self.timeout and not self.triggered then
+    self.count = self.count + 1
+    self:check()
+  elseif not self.triggered then
+    self.count = 1
+  end
+  self.last = curtime
+end
+
+local function early_stop(word, word_eol, userdata)
+  if #word < 2 then
+    print("I require a watcher to disable")
+    return
+  end
+  local watcher = watchers[word[2]]
+  if watcher == nil then
+    print(string.format("Watcher %s not found in watcher list.", word[2]))
+    return
+  end
+  print(string.format("Disabling watcher %s", word[2]))
+  watcher:disable()
+end
+
+
+-- TODO: make this doable on the fly. The ability to mark something specific as spammable is really handy.
+watchers["ping"] = WatcherClass:new("ping", { "input_balloon_hilight", "input_beep_hilight", "input_flash_hilight", "input_tray_hilight" }, "Ping spam detected, enabling countermeasures")
+watchers["pm"] = WatcherClass:new("pm", { "input_balloon_priv", "input_beep_priv", "input_flash_priv", "input_tray_priv" }, "PM spam detected, enabling countermeasures")
+hexchat.hook_print("Private Message To Dialog", function() watchers["pm"]:trigger() end)
+hexchat.hook_print("Channel Msg Hilight", function() watchers["ping"]:trigger() end)
+hexchat.hook_print("Channel Action Hilight", function() watchers["ping"]:trigger() end)
+hexchat.hook_command("stoptimeout", early_stop)
+
+hexchat.hook_unload(function() print(plugin_name, "unloaded") end)
+print(plugin_name, "loaded")
